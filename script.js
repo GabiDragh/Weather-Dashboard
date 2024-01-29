2df5b97a17b118929c95f2f069f3ed50ef844f4d
Gabi Draghici <draghici.gabi89@gmail.com>
2024-01-29 00:31:31 +0000

    Today weather section added in script.js

--- script.js
+++ script.js
@@ -1,71 +1,113 @@
 //When a user searches for a city they are presented with current and future conditions for that city and that city is added to the search history:
 
-//1. Fetch openweathermap.org current and future weather information for the city searched (fetch function)
+//Fetch openweathermap.org current and future weather information for the city searched (fetch function)
 
 function getTodaysWeather(){
 
-//Create search input variable to use as dynamic input in location URL 
-var searchInput = $("#search-input").val();
-console.log(searchInput);
+  //Create search input variable to use as dynamic input in location URL 
+  var searchInput = $("#search-input").val();
+  console.log(searchInput);
 
-//Create API key variable to use for both today's conditions and 5-day forecast URLs:
-var APIkey = "5dd9042c49a35a443a574925136ee109";
+  //Create API key variable to use for both today's conditions and 5-day forecast URLs:
+  var APIkey = "5dd9042c49a35a443a574925136ee109";
 
-//Create the URL needed to access the current weather database, using the API key:
+  //Create the URL needed to access the current weather database, using the API key:
 
-var currentLocationURL = "http://api.openweathermap.org/geo/1.0/direct?q="+searchInput+"&limit=1&appid=" + APIkey;
-console.log(currentLocationURL);
+  var currentLocationURL = "http://api.openweathermap.org/geo/1.0/direct?q="+searchInput+"&limit=2&appid=" + APIkey;
+  console.log(currentLocationURL);
 
-//Run fetch call for the URL
-fetch(currentLocationURL)
- .then(function (response) {
-//Ask for .json format data
- return response.json();
-})
-//Function that stores all data
-.then(function (data) {
+  //Run fetch call for the URL
+  fetch(currentLocationURL)
+   .then(function (response) {
 
-var currentLat = JSON.stringify(data[0].lat);
-var currentLong = JSON.stringify(data[0].lon);
+  //Ask for .json format data
+   return response.json();
+  })
 
-console.log(data);
-console.log(currentLat);
-console.log(currentLong);
+  //Function that stores all data
+  .then(function (data) {
 
-var currentWeatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + currentLat + "&lon=" + currentLong + "&units=metric&appid=" + APIkey;
-console.log(currentWeatherURL);
+  var currentLat = JSON.stringify(data[0].lat); //variable to extract latitude information to use in the 5-day weather API URL
+  var currentLong = JSON.stringify(data[0].lon); //variable to extract longitude information to use in the 5-day weather API URL
 
-fetch(currentWeatherURL)
+  console.log(data);
+  console.log(currentLat);
+  console.log(currentLong);
 
-.then(function (response) {
-    return response.json();
-})
-.then(function (weatherData){
-    console.log(weatherData);
-})
+  //Create 5-day data API URL
+  var currentWeatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + currentLat + "&lon=" + currentLong + "&units=metric&appid=" + APIkey;
+  console.log(currentWeatherURL);
 
-})
+  //Current weather conditions to display in #today: city name, date, weather conditions icon, temperature, humidity, wind speed (create element, add content and append for each value)
+  //Fetch object data
+  fetch(currentWeatherURL)
+
+  //Ask for JSON format data
+  .then(function (response) {
+     return response.json();
+    })
+  //Function that stores all data
+     .then(function (weatherData) {
+     console.log(weatherData);
+    
+     //In #today HTML section, create a header div containing the name, date and the icon, then add the next lines of information (temperature, humidity, wind speed)
+     
+     var headerToday = $("<h1>"); //create header div
+
+     var city = weatherData.city.name;
+     console.log(city);
+     var date = dayjs().format('DD/MM/YYYY');
+     console.log(date);
+     var iconCode = weatherData.list[0].weather[0].icon;
+     console.log(iconCode);
+     var iconURL = "https://openweathermap.org/img/wn/"+ iconCode +"@2x.png";
+     console.log(iconURL);
+     var icon = $("<img>").attr("src", iconURL)
+     console.log(icon);
+
+     $(headerToday).append(city, " ", date, " ", icon);
+
+     var temp = weatherData.list[0].main.temp;
+     console.log(temp);
+     var displayTemp = $("<p>").text("Temp: " + temp + " \u00B0C");
+
+    var wind = weatherData.list[0].wind.speed;
+    console.log(wind);
+    var displayWind = $("<p>").text("Wind: " + wind + " KPH");
+
+
+    var humidity = weatherData.list[0].main.humidity;
+    console.log(humidity);
+    var displayHumidity = $("<p>").text("Humidity: " + humidity + " %");
+
+     $("#today").append(headerToday, displayTemp, displayWind, displayHumidity); //append header to the html section
+
+    })
+
+ })
 
 }
-
 getTodaysWeather();
 
-//Current weather conditions to display in #today: city name, date, weather conditions icon, temperature, humidity, wind speed (create element, add content and append for each value)
+
+
 
 //Future weather conditions are shown in a 5 day forecast displaying in #forecast: the date, icon for weather conditions, temperature and humidity (create 5 bootstrap cards in the div for each day, then create element, add content and append for each value needed?)
 
-//2. Add searched city into local storage - #history, .list-group (event listener on the search button - function to create button for city and store as array)
+//2. Add searched city into local storage - #history/.list-group (event listener on the search button - function to create button for city and store as array)
 var searches = JSON.parse(localStorage.getItem("searches"))||[];
 
 $("#search-button").on("click", function (event) {
-    event.preventDefault();
-
-    var location = $("#search-input").val().trim();
-    searches.push(location);
+    event.preventDefault(); 
+    $("#today").empty(); //Clear the weather info displayed when search button is pressed again
+    //$("#forecast").empty();  TODO: add clear weather info for #forecast once section finished
+    var location = $("#search-input").val().trim(); //define variable to store the user input value
+    searches.push(location); //push the input to the searches array
     console.log(searches);
 
-    addButton();
-    getTodaysWeather();
+    addButton(); //create a button for the new search
+    getTodaysWeather(); //display the weather
+    $("#search-input").val(""); //clear the input value 
      //store in local storage
      localStorage.setItem("searches", JSON.stringify(searches));
 })
@@ -80,14 +122,17 @@ function addButton() {
         newButton.addClass("searchList"); //add class content
         newButton.attr("data-name", searches[i]); //retrieve data attribute
         newButton.text(searches[i]); //add text content
-        $("#history").append(newButton); //append button to the html div
+        $("#history").prepend(newButton); //append button to the html div
        
         
     }
 }
 
-function previousSearchInfo() {
-    //get info from local storage
-    //run getTodaysWeather
-}
+
+
 //When the user presses the stored city button, the app displays the stats again (getItem from local storage, add event listener that calls the show temperature function)
+// function previousSearchInfo() {
+//     //event listener for button press->//get info from local storage
+//     //run getTodaysWeather
+// }
+// previousSearchInfo();
